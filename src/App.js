85aafc5fac269bcd641af9b68daaf033930b6502
mauricio-contin <128005385+mauricio-contin@users.noreply.github.com>
2023-04-05 20:24:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,13 +12,7 @@ class App extends Component {
           <h1 className="App-title">Sem risadinha</h1>
         </header>
         <p className="App-intro">
-          premissa:
-substantivo feminino
-1.
-LÓGICA
-cada uma das proposições que compõem um silogismo e em que se baseia a conclusão.
-2.
-ponto ou ideia de que se parte para armar um raciocínio.</code> and save to reload.
+          premissa:substantivo feminino, cada uma das proposições que compõem um silogismo e em que se baseia a conclusão.Ponto ou ideia de que se parte para armar um raciocínio.</code> and save to reload.
         </p>
       </div>
     );
