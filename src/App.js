816c7ab7e7f65394c8efdd5b6a66e475433130cf
mauricio-contin <128005385+mauricio-contin@users.noreply.github.com>
2023-04-05 20:03:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import mba from './MBA.png';
+import mba from './koba.png';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to FIAP 43SCJ</h1>
+          <h1 className="App-title">Sem risadinha</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
