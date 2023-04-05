edc17a4df3c5856064f88fb327f0242916736831
mauricio-contin <128005385+mauricio-contin@users.noreply.github.com>
2023-04-05 20:08:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import mba from './koba.png';
+import mba from './koba.jpg';
 import './App.css';
 
 class App extends Component {
